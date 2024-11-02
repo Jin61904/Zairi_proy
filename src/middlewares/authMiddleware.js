2220b720cd6fa4966e2acc8c0231fecad737beb1
Jin61904 <migueld.ruizs@konradlorenz.edu.co>
2024-11-01 19:45:56 -0500

    Se crea middleware para la autenticación de usuarios.

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    if (!token) return res.status(401).json({ message: 'Acceso denegado, no se proporcionó token' });
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Token no válido' });
+    }
+};
+
+module.exports = authMiddleware;
