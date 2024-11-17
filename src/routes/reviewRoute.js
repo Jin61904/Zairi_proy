3cbfea99bd1b1c5eeb03499de827acd498c9a050
Camila Galindo <galindo.camilaandrea@gmail.com>
2024-11-17 17:03:08 -0500

    se cambia en rutas de get la información traída.

--- src/routes/reviewRoute.js
+++ src/routes/reviewRoute.js
@@ -18,7 +18,9 @@ router.post('/review', auth, async (req, res) => {
 // Obtener todas las reseñas
 router.get('/review', async (req, res) => {
     try {
-        const reviews = await Review.find().populate('userId serviceId');
+        const reviews = await Review.find()
+            .populate('userId', 'name email') // Limitar campos de userId
+            .populate('serviceId', 'type description price'); // Limitar campos de serviceId
         res.status(200).json(reviews);
     } catch (error) {
         res.status(500).json({ message: 'Error al obtener las reseñas', error });
@@ -28,7 +30,9 @@ router.get('/review', async (req, res) => {
 // Obtener una reseña por ID
 router.get('/review/:id', async (req, res) => {
     try {
-        const review = await Review.findById(req.params.id).populate('userId serviceId');
+        const review = await Review.findById(req.params.id)
+            .populate('userId', 'name email') // Limitar campos de userId
+            .populate('serviceId', 'type description price'); // Limitar campos de serviceId
         if (!review) return res.status(404).json({ message: 'Reseña no encontrada' });
         res.status(200).json(review);
     } catch (error) {
