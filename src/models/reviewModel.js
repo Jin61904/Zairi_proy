0e8efb6fbfc0aeace66d1c6784ed9c29ebafcd79
Jin61904 <migueld.ruizs@konradlorenz.edu.co>
2024-11-26 21:12:53 -0500

    Se dejan requeridos campos de titulo, commentario y calificación en review

--- src/models/reviewModel.js
+++ src/models/reviewModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const reviewSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }, 
     serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: false }, 
-    title: { type: String, required: false },
-    rating: { type: Number, required: false, min: 1, max: 5 }, 
-    comment: { type: String, required: false }, 
+    title: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 }, 
+    comment: { type: String, required: true }, 
     createdAt: { type: Date, default: Date.now }
 });
 
