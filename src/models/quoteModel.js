335a84cb23564fdcb985f8ae583dd305bd971d07
Jin61904 <migueld.ruizs@konradlorenz.edu.co>
2024-11-30 11:58:01 -0500

    Se quita .env de gitignore

--- src/models/quoteModel.js
+++ src/models/quoteModel.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const quoteSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }, 
-    serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: false }, 
+    serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: false },
     details: { type: String, required: true }, 
-    estimatedPrice: { type: Number }, 
-    status: { type: String, default: 'Pendiente' }, 
+    estimatedPrice: { type: Number, required: false }, 
+    status: { type: String, default: 'Pendiente' },
+    cellphone: { type: String, required: false },
     createdAt: { type: Date, default: Date.now }
 });
 
